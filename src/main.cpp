07b6c28cfac93c8e6476254a620ce1bef0d68b86
Tomasz Zymkowski <33435149+Zymoo@users.noreply.github.com>
2018-05-25 21:44:30 +0200

    Update main.cpp
    Added sending port number to new client

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "uWS/uWS.h"
 #include <iostream>
 #include <string.h>
@@ -13,11 +14,11 @@ class Queue {
   std::vector<State> stateHistory;
   int numberClient;
   // send current state to all client
-  void broadcast(State currentState){};
+  void broadcast(State currentState) {};
 
 public:
-  Queue(){};
-  ~Queue(){};
+  Queue() {};
+  ~Queue() {};
   // checks all queue's loops, when current state is ready fire broadcast(), and
   // saves it to stateHistory
   void queueLoop() {
@@ -32,11 +33,11 @@ public:
       }
     }
   };
-  void addNewClient(int port){
-      // add new queues
+  void addNewClient(int port) {
+    // add new queues
   };
-  void handleMessage(){
-      // add messeage to queses
+  void handleMessage() {
+    // add messeage to queses
   };
 };
 
@@ -45,39 +46,51 @@ void clientMain(int port, Queue &queue) {
   uWS::Hub h;
 
   h.onConnection(
-      [port, &queue](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
-        std::cout << "server thread: connected on port " << port << '\n';
-        queue.addNewClient(port);
-      });
+    [port, &queue](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
+    std::cout << "server thread: connected on port " << port << '\n';
+    queue.addNewClient(port);
+  });
   h.onMessage([&queue](uWS::WebSocket<uWS::SERVER> *ws, char *message,
-                       size_t length,
-                       uWS::OpCode opCode) { queue.handleMessage(); });
+    size_t length,
+    uWS::OpCode opCode) { queue.handleMessage(); });
 
   h.listen(port);
   h.run();
+}
+
+void copyPortNum(int number, char* string) {
+
+  for (int i = 0; i < 4; i++) {
+    string[3 - i] = number % 10 + '0';
+    number = number / 10;
+  }
 }
 
 int main() {
 
   // main thread waits on port 3000 for clients, when client connects new thread
   // starts running and handles the client on new port
+
   uWS::Hub h;
   Queue q = Queue();
   int clientId = 0;
-  int clientPort[] = {3001, 3002, 3003, 3004, 3005, 3006,
-                      3007, 3008, 3009, 3010, 3011, 3012};
+  int clientPort[] = { 3001, 3002, 3003, 3004, 3005, 3006,
+    3007, 3008, 3009, 3010, 3011, 3012 };
   std::vector<std::thread> threadVector;
 
-  h.onConnection([&q, &clientId, clientPort, &threadVector](
-                     uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
+  h.onConnection([&q, &clientId, clientPort, &threadVector](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) 
+  {
     std::cout << "server main: connected with client " << clientId << '\n';
+    char *message = new char[4];
+    copyPortNum(clientPort[clientId], message);
+    ws->send(message, 4, uWS::OpCode::TEXT);
+    threadVector.push_back(std::thread(&clientMain, clientPort[clientId], std::ref(q)));
     clientId++;
-    threadVector.push_back(
-        std::thread(clientMain, clientPort[clientId], std::ref(q)));
   });
+
 
   h.listen(3000);
   h.run();
-
+  
   return 0;
 }
